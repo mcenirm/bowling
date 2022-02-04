3c727bf434b765ec6a97b4d04856f6a4c5642e8a
Michael McEniry <mcenirm@mceniry.net>
2022-02-04 13:40:53 -0600

    function parameters with descriptions

--- bowl.py
+++ bowl.py
@@ -109,9 +109,25 @@ def test_build_tree():
 
 
 def description_and_parameters_from_function(func: typing.Callable) -> tuple[str, list]:
-    lines = textwrap.dedent(func.__doc__).splitlines()
+    func_desc = ""
+    param_descs = {}
+    if func.__doc__:
+        lines = textwrap.dedent(func.__doc__).splitlines()
+        func_desc = lines[0]
+        for line in lines:
+            pd = parameter_name_and_description_from_docstring_line(line)
+            if pd:
+                pname, pdesc = pd
+                param_descs[pname] = pdesc
     spec = inspect.getfullargspec(func)
-    return lines[0], [[_] for _ in spec.args]
+    params = []
+    for pname in spec.args:
+        if pname in param_descs:
+            param = [pname, {"description": param_descs.pop(pname)}]
+        else:
+            param = [pname]
+        params.append(param)
+    return func_desc, params
 
 
 def test_description_and_parameters_from_function():
@@ -127,13 +143,50 @@ def test_description_and_parameters_from_function():
 
     def function_with_parameters_but_no_details(a, b, c):
         """This function has parameters but does not explain them"""
-        ...
 
     d, p = description_and_parameters_from_function(
         function_with_parameters_but_no_details
     )
     assert d == function_with_parameters_but_no_details.__doc__
     assert p == [[_] for _ in "abc"]
+
+    def function_with_parameters_with_details(a, b, c):
+        """This function has parameters with details in docstring
+
+        a: description for a
+        b: description for b
+        c: description for c"""
+
+    d, p = description_and_parameters_from_function(
+        function_with_parameters_with_details
+    )
+    assert d == "This function has parameters with details in docstring"
+    assert p == [[_, dict(description=f"description for {_}")] for _ in "abc"]
+
+
+def parameter_name_and_description_from_docstring_line(
+    line: str,
+) -> typing.Optional[tuple[str, str]]:
+    parts = line.split(":", maxsplit=1)
+    if len(parts) == 2:
+        left, right = tuple([_.strip() for _ in parts])
+        if left.isidentifier():
+            return left, right
+    return None
+
+
+def test_parameter_name_and_description_from_docstring_line():
+    import pytest
+
+    for line in ["", "no marker"]:
+        assert parameter_name_and_description_from_docstring_line(line) is None
+    for left in ["example"]:
+        for right in ["a description"]:
+            line = f"{left}: {right}"
+            assert parameter_name_and_description_from_docstring_line(line) == (
+                left.strip(),
+                right.strip(),
+            )
 
 
 def parse_args(args: list[str] = sys.argv[1:]):
