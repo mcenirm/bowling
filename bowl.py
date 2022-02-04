61a5721626673ed7338bc4f97b128aa215fddb92
Michael McEniry <mcenirm@mceniry.net>
2022-02-04 15:11:57 -0600

    argument details as instanced objects

--- bowl.py
+++ bowl.py
@@ -1,4 +1,5 @@
 import argparse
+import dataclasses
 import inspect
 import sys
 import textwrap
@@ -25,7 +26,7 @@ def bowl():
     ...
 
 
-########## development commands ##########
+# region development commands
 
 
 def dev_init():
@@ -39,7 +40,51 @@ def dev_pytest():
     ...
 
 
-########## argument parsing ##########
+# endregion
+
+
+# region argument parsing
+
+
+@dataclasses.dataclass
+class ArgumentParserAddArgumentArguments:
+    # Either a name or a list of option strings, e.g. foo or -f, --foo.
+    name_or_flags: list[str] = dataclasses.field(default_factory=list)
+
+    # The basic type of action to be taken when this argument is encountered at the command line.
+    # action: typing.Union[str, typing.Type[argparse.Action]]
+
+    # The number of command-line arguments that should be consumed.
+    # nargs: typing.Union[int, str]
+
+    # A constant value required by some action and nargs selections.
+    # const: typing.Any
+
+    # The value produced if the argument is absent from the command line and if it is absent from the namespace object.
+    # default: typing.Any
+
+    # The type to which the command-line argument should be converted.
+    # type: typing.Union[typing.Callable[[str], argparse._T @ add_argument], argparse.FileType]
+
+    # A container of the allowable values for the argument.
+    # choices: typing.Union[typing.Iterable[argparse._T @ add_argument], None]
+
+    # Whether or not the command-line option may be omitted (optionals only).
+    # required: bool
+
+    # A brief description of what the argument does.
+    # help: typing.Union[str, None]
+
+    # A name for the argument in usage messages.
+    # metavar: typing.Union[str, tuple[str, ...], None]
+
+    # The name of the attribute to be added to the object returned by parse_args().
+    # dest: typing.Union[str, None]
+
+    # ...
+    # version: str
+
+    kwargs: dict = dataclasses.field(default_factory=dict)
 
 
 def build_arg_parser(
@@ -108,7 +153,9 @@ def test_build_tree():
         build_tree([1, 2, 3], lambda n: ["x"] * n)
 
 
-def description_and_parameters_from_function(func: typing.Callable) -> tuple[str, list]:
+def description_and_parameters_from_function(
+    func: typing.Callable,
+) -> tuple[str, list[ArgumentParserAddArgumentArguments]]:
     func_desc = ""
     param_descs = {}
     if func.__doc__:
@@ -122,16 +169,19 @@ def description_and_parameters_from_function(func: typing.Callable) -> tuple[str
     spec = inspect.getfullargspec(func)
     params = []
     for pname in spec.args:
+        param = ArgumentParserAddArgumentArguments(name_or_flags=[pname])
         if pname in param_descs:
-            param = [pname, {"description": param_descs.pop(pname)}]
-        else:
-            param = [pname]
+            param.kwargs["help"] = param_descs.pop(pname)
         params.append(param)
     return func_desc, params
 
 
 def test_description_and_parameters_from_function():
-    """TODO test functions with parameters"""
+    """TODO
+    [x] no parameters
+    [x] parameters but no descriptions
+    [x] parameters with descriptions
+    [ ] parameters as typed objects"""
     import pytest
 
     def function_without_parameters():
@@ -148,20 +198,28 @@ def test_description_and_parameters_from_function():
         function_with_parameters_but_no_details
     )
     assert d == function_with_parameters_but_no_details.__doc__
-    assert p == [[_] for _ in "abc"]
+    assert p == [ArgumentParserAddArgumentArguments(name_or_flags=[_]) for _ in "abc"]
 
     def function_with_parameters_with_details(a, b, c):
         """This function has parameters with details in docstring
 
-        a: description for a
-        b: description for b
-        c: description for c"""
+        a: help message for a
+        b: help message for b
+        c: help message for c"""
 
     d, p = description_and_parameters_from_function(
         function_with_parameters_with_details
     )
     assert d == "This function has parameters with details in docstring"
-    assert p == [[_, dict(description=f"description for {_}")] for _ in "abc"]
+    assert p == [
+        ArgumentParserAddArgumentArguments(
+            name_or_flags=[_],
+            kwargs=dict(
+                help=f"help message for {_}",
+            ),
+        )
+        for _ in "abc"
+    ]
 
 
 def parameter_name_and_description_from_docstring_line(
@@ -193,7 +251,10 @@ def parse_args(args: list[str] = sys.argv[1:]):
     return build_arg_parser().parse_args(args=args)
 
 
-########## CLI driver ##########
+# endregion
+
+
+# region CLI driver
 
 
 def run_args(args):
@@ -210,3 +271,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# endregion
